ceb8ae674655cc823588f0773f51add4b1237d64
Pedro Eugenio <enviaparaopedro@gmail.com>
2023-10-20 23:49:08 -0300

    Criado findById

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,23 +1,20 @@
 package application;
 
-import java.util.Date;
-
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Department obj = new Department(1, "Livros");
-		System.out.println(obj);
-		
-		Seller seller = new Seller(21, "Pedro", "emai@gmail.com",new Date(), 3000.0, obj);
+//		Department obj = new Department(1, "Livros");
+//		System.out.println(obj);
+//		
+//		Seller seller = new Seller(21, "Pedro", "emai@gmail.com",new Date(), 3000.0, obj);
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
-		
+		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
 
 	}
