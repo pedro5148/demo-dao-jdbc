92bfabc2f98652b2383881d460f57f6bda6e2965
Pedro Eugenio <enviaparaopedro@gmail.com>
2023-10-20 23:55:52 -0300

    Reusando as instanciacoes

--- src/application/Program.java
+++ src/application/Program.java
@@ -14,6 +14,8 @@ public class Program {
 //		Seller seller = new Seller(21, "Pedro", "emai@gmail.com",new Date(), 3000.0, obj);
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
+		System.out.println("=== Teste 1: Seller findById ===");
 		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
 
